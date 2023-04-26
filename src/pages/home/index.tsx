231ead583baed40b93850dccd76517f348b4a812
lucasmitsuomandai <lucas.mandai@lmandai.com>
2023-04-26 00:27:43 -0300

    chore: building hilights

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,12 @@
 import Head from 'next/head'
-import { DefaultContainer, MenuContainer, MenuItem } from './styles'
+import Image from 'next/image'
+import boloCenoura from '../../assets/recipes/boloLaranja.jpg'
+import {
+  DefaultContainer,
+  HighlightsContainer,
+  MenuContainer,
+  MenuItem,
+} from './styles'
 import { FaHamburger } from 'react-icons/fa'
 import Link from 'next/link'
 
@@ -15,24 +22,56 @@ export default function Home() {
             <MenuItem>
               <FaHamburger size={24} />
             </MenuItem>
+            <p>Bebidas</p>
+          </Link>
+          <Link href={'/'}>
+            <MenuItem>
+              <FaHamburger size={24} />
+            </MenuItem>
             <p>Lanches</p>
           </Link>
-          <MenuItem>
-            <FaHamburger size={24} />
-          </MenuItem>
-          <MenuItem>
-            <FaHamburger size={24} />
-          </MenuItem>
-          <MenuItem>
-            <FaHamburger size={24} />
-          </MenuItem>
-          <MenuItem>
-            <FaHamburger size={24} />
-          </MenuItem>
-          <MenuItem>
-            <FaHamburger size={24} />
-          </MenuItem>
+          <Link href={'/'}>
+            <MenuItem>
+              <FaHamburger size={24} />
+            </MenuItem>
+            <p>Lanches</p>
+          </Link>
+          <Link href={'/'}>
+            <MenuItem>
+              <FaHamburger size={24} />
+            </MenuItem>
+            <p>Lanches</p>
+          </Link>
+          <Link href={'/'}>
+            <MenuItem>
+              <FaHamburger size={24} />
+            </MenuItem>
+            <p>Lanches</p>
+          </Link>
+          <Link href={'/'}>
+            <MenuItem>
+              <FaHamburger size={24} />
+            </MenuItem>
+            <p>Lanches</p>
+          </Link>
         </MenuContainer>
+
+        <HighlightsContainer>
+          <div className="mainItem">
+            <Link href="">
+              <Image src={boloCenoura} alt="" placeholder="blur"></Image>
+              <span>Bolo de Cenoura</span>
+            </Link>
+          </div>
+          <div className="secondaryItemContainer">
+            <div className="secondaryItem">
+              <Image src="" alt=""></Image>
+            </div>
+            <div className="secondaryItem">
+              <Image src="" alt=""></Image>
+            </div>
+          </div>
+        </HighlightsContainer>
       </DefaultContainer>
     </>
   )
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -20,8 +20,57 @@ export const MenuItem = styled('div', {
   borderRadius: '50%',
   background: 'white',
   boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.2)',
+  marginBottom: '0.5rem',
 
   svg: {
     color: '$blueTheme',
+  },
+})
+
+export const HighlightsContainer = styled('section', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '3rem',
+  width: '100%',
+  height: '100%',
+  marginTop: '2.5rem',
+
+  '.mainItem': {
+    minWidth: '40rem',
+    height: '400px',
+    position: 'relative',
+
+    img: {
+      borderRadius: '8px',
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      filter: 'contrast(0.8)',
+    },
+    span: {
+      position: 'absolute',
+      top: '80%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: '2rem',
+      textAlign: 'center',
+      textTransform: 'uppercase',
+    },
+  },
+
+  '.secondaryItemContainer': {
+    minWidth: '20rem',
+    border: '1px solid $blueTheme',
+
+    '.secondaryItem': {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
   },
 })
